f2372ba8dc11d8dada8ce2686a8f8c776ce45097
christianLizama <ckill661@gmail.com>
2023-12-10 18:06:08 -0300

    entrenar mediante parametros de entrada

--- src/train.py
+++ src/train.py
@@ -6,15 +6,42 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pickle
 import os
+import sys
 
 # Cargar el conjunto de datos Iris
 iris = load_iris()
 
-# Dividir el conjunto de datos en un conjunto de entrenamiento y un conjunto de prueba
-X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.3, random_state=1)
+# Parámetros predeterminados
+test_size = None
+random_state = None
+n_neighbors = None
 
-# Crear un modelo KNN con k=3
-knn = KNeighborsClassifier(n_neighbors=3)
+# Obtener los parámetros de entrada desde la consola o usar valores predeterminados
+args = iter(sys.argv[1:])
+next_arg = None
+
+for arg in args:
+    if arg.startswith("--"):
+        next_arg = arg
+    elif next_arg == "--test_size":
+        test_size = float(arg)
+        next_arg = None
+    elif next_arg == "--random_state":
+        random_state = int(arg)
+        next_arg = None
+    elif next_arg == "--n_neighbors":
+        n_neighbors = int(arg)
+        next_arg = None
+
+if test_size is None or random_state is None or n_neighbors is None:
+    print("Error: Todos los parámetros (--test_size, --random_state, --n_neighbors) son requeridos.")
+    sys.exit(1)
+
+# Dividir el conjunto de datos en un conjunto de entrenamiento y un conjunto de prueba
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=test_size, random_state=random_state)
+
+# Crear un modelo KNN con el número de vecinos especificado
+knn = KNeighborsClassifier(n_neighbors=n_neighbors)
 
 # Entrenar el modelo KNN en el conjunto de entrenamiento
 knn.fit(X_train, y_train)
